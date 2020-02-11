d913637ea6b34361dddc42c84c5d0349c0322d30
Zarudensky <37154263+Zarudensky@users.noreply.github.com>
2020-02-11 12:02:58 -0800

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -5,7 +5,7 @@ function windowSize(){
 			centerMode: true,
 			centerPadding: '80px',
 			autoplay: true,
-			autoplaySpeed: 2500,
+			autoplaySpeed: 2000,
 			pauseOnHover: false,
 			dots: false,
 			arrows: false,
