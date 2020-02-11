4839515b258a7ac49072686fc23a16cae9534787
Zarudensky <37154263+Zarudensky@users.noreply.github.com>
2020-02-11 07:39:09 -0800

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+function windowSize(){
+    if ($(window).width() <= '560'){
+        $('.services__container').addClass('slider');
+        $('.slider').slick({
+			centerMode: true,
+			centerPadding: '80px',
+			autoplay: true,
+			autoplaySpeed: 3000,
+			pauseOnHover: false,
+			dots: false,
+			arrows: false,
+			responsive: [
+		    {
+		      breakpoint: 400,
+		      settings: {
+		        centerPadding: '60px',
+		      }
+		    },
+		    {
+		      breakpoint: 360,
+		      settings: {
+		        centerPadding: '40px',
+		      }
+		    }
+		  ]
+		});
+    }
+}
+$(window).on('load',windowSize);
